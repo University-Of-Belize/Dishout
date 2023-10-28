5c42966f1fe410bb09aa13ba6270b80c15e01049
AlexDev404 <alexdev404@users.noreply.github.com>
2023-10-28 17:31:01 -0600

    WhatIs/Format: `Authentication.ts`

--- src/api/utility/Authentication.ts
+++ src/api/utility/Authentication.ts
@@ -1,6 +1,4 @@
-import User from "../../database/models/Users"
-function get_authorization(req: Express.Request) {
+import User from "../../database/models/Users";
+function get_authorization(req: Express.Request) {}
 
-}
-
-export {get_authorization}
+export { get_authorization };
---
+++ src/api/utility/What_Is.ts
@@ -0,0 +1,25 @@
+// What is typescript?
+/*
+what_is_response
+==================
+{
+what: “object”,  // What
+[“property”, “property”, ...]  // Is
+}
+*/
+
+class WhatIs {
+  what: string;
+  is: any;
+
+  constructor(what: string, is: any) {
+    this.what = what;
+    this.is = is;
+  }
+}
+
+function what_is(what: string, is: any) {
+  return new WhatIs(what, is);
+}
+
+export { what_is };
