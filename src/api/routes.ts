20ac22fc0e38873546732aa537cea388872be391
AlexDev404 <alexdev404@users.noreply.github.com>
2023-10-30 04:29:57 -0600

    Fixed the routes type declarations

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,5 +1,14 @@
 import { Router } from "express"; // Our routing machine
-import { Admin, Authentication, Menu, Order, User, Category, Review } from "./models"; // Import our API models into memory
+import {
+  Admin,
+  Authentication,
+  Menu,
+  Order,
+  User,
+  Category,
+  Review,
+} from "./models"; // Import our API models into memory
+import { Request, Response } from "express";
 import { LogRoutes } from "../util/Logger";
 const router = Router(); // Initialize!
 
@@ -14,170 +23,113 @@ router.patch("/auth/passwordreset", Authentication.Reset);
 
 // Admin-API
 // User
-router.get(
-  "/admin/user/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.User.List(req, res);
-  }
-);
-router.post(
-  "/admin/user/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.User.Create(req, res);
-  }
-);
-router.delete(
-  "/admin/user/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.User.Delete(req, res);
-  }
-);
-router.put(
-  "/admin/user/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.User.Modify(req, res);
-  }
-);
+router.get("/admin/user/manage", (req: Request, res: Response) => {
+  Admin.User.List(req, res);
+});
+router.post("/admin/user/manage", (req: Request, res: Response) => {
+  Admin.User.Create(req, res);
+});
+router.delete("/admin/user/manage", (req: Request, res: Response) => {
+  Admin.User.Delete(req, res);
+});
+router.put("/admin/user/manage", (req: Request, res: Response) => {
+  Admin.User.Modify(req, res);
+});
 
 // Promo
-router.get(
-  "/admin/promo/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Promo.List(req, res);
-  }
-);
-router.post(
-  "/admin/promo/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Promo.Create(req, res);
-  }
-);
-router.delete(
-  "/admin/promo/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Promo.Delete(req, res);
-  }
-);
-router.put(
-  "/admin/promo/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Promo.Modify(req, res);
-  }
-);
+router.get("/admin/promo/manage", (req: Request, res: Response) => {
+  Admin.Promo.List(req, res);
+});
+router.post("/admin/promo/manage", (req: Request, res: Response) => {
+  Admin.Promo.Create(req, res);
+});
+router.delete("/admin/promo/manage", (req: Request, res: Response) => {
+  Admin.Promo.Delete(req, res);
+});
+router.put("/admin/promo/manage", (req: Request, res: Response) => {
+  Admin.Promo.Modify(req, res);
+});
 // Category
-router.post(
-  "/admin/category/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Category.Create(req, res);
-  }
-);
-router.delete(
-  "/admin/category/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Category.Delete(req, res);
-  }
-);
-router.put(
-  "/admin/category/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Category.Modify(req, res);
-  }
-);
+router.post("/admin/category/manage", (req: Request, res: Response) => {
+  Admin.Category.Create(req, res);
+});
+router.delete("/admin/category/manage", (req: Request, res: Response) => {
+  Admin.Category.Delete(req, res);
+});
+router.put("/admin/category/manage", (req: Request, res: Response) => {
+  Admin.Category.Modify(req, res);
+});
 // Order
 // Accept user's order into queue
-router.post(
-  "/admin/order/manage/",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Order.Modify(req, res);
-  }
-);
+router.post("/admin/order/manage/", (req: Request, res: Response) => {
+  Admin.Order.Modify(req, res);
+});
 // Decline user's order into queue
-router.delete(
-  "/admin/order/manage/",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Order.Modify(req, res);
-  }
-);
+router.delete("/admin/order/manage/", (req: Request, res: Response) => {
+  Admin.Order.Modify(req, res);
+});
 // Modify user's order in queue
-router.put(
-  "/admin/order/manage/",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Order.Modify(req, res);
-  }
-);
+router.put("/admin/order/manage/", (req: Request, res: Response) => {
+  Admin.Order.Modify(req, res);
+});
 // Review
-router.delete(
-  "/admin/review/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Menu.Delete(req, res);
-  }
-);
-router.put(
-  "/admin/review/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Menu.Modify(req, res);
-  }
-);
+router.delete("/admin/review/manage", (req: Request, res: Response) => {
+  Admin.Menu.Delete(req, res);
+});
+router.put("/admin/review/manage", (req: Request, res: Response) => {
+  Admin.Menu.Modify(req, res);
+});
 // Menu
-router.post(
-  "/admin/menu/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Menu.Create(req, res);
-  }
-);
-router.delete(
-  "/admin/menu/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Menu.Delete(req, res);
-  }
-);
-router.put(
-  "/admin/menu/manage",
-  (req: Express.Request, res: Express.Response) => {
-    Admin.Menu.Modify(req, res);
-  }
-);
+router.post("/admin/menu/manage", (req: Request, res: Response) => {
+  Admin.Menu.Create(req, res);
+});
+router.delete("/admin/menu/manage", (req: Request, res: Response) => {
+  Admin.Menu.Delete(req, res);
+});
+router.put("/admin/menu/manage", (req: Request, res: Response) => {
+  Admin.Menu.Modify(req, res);
+});
 
 // User-API
 // Menu
-router.get("/menu/", (req: Express.Request, res: Express.Response) => {
+router.get("/menu/", (req: Request, res: Response) => {
   Menu.List(req, res);
 });
 
-router.get("/menu/search", (req: Express.Request, res: Express.Response) => {
+router.get("/menu/search", (req: Request, res: Response) => {
   Menu.Lookup(req, res); // Users can lookup other menu items
 });
 // Category
-router.get("/category/", (req: Express.Request, res: Express.Response) => {
+router.get("/category/", (req: Request, res: Response) => {
   Category.List(req, res);
 });
 // Order
 // Order History. Arbitrary tokens are accepted
-router.get("/order/history", (req: Express.Request, res: Express.Response) => {
+router.get("/order/history", (req: Request, res: Response) => {
   Order.List(req, res);
 });
 // Users can lookup their orders
-router.get("/order/search", (req: Express.Request, res: Express.Response) => {
-  Order.Lookup(req, res); 
+router.get("/order/search", (req: Request, res: Response) => {
+  Order.Lookup(req, res);
 });
 
 // Order placement. Arbitrary tokens are accepted
-router.post("/order/place", (req: Express.Request, res: Express.Response) => {
+router.post("/order/place", (req: Request, res: Response) => {
   Order.Create(req, res);
 });
-router.delete("/order/place", (req: Express.Request, res: Express.Response) => {
+router.delete("/order/place", (req: Request, res: Response) => {
   Order.Delete(req, res);
 });
-router.put("/order/place", (req: Express.Request, res: Express.Response) => {
+router.put("/order/place", (req: Request, res: Response) => {
   Order.Modify(req, res);
 });
 
 // Review
-router.post("/review/create", (req: Express.Request, res: Express.Response) => {
+router.post("/review/create", (req: Request, res: Response) => {
   Review.Create(req, res);
 });
 // User
-router.get("/user/search", (req: Express.Request, res: Express.Response) => {
+router.get("/user/search", (req: Request, res: Response) => {
   User.Lookup(req, res); // Users can lookup other users
 });
 
