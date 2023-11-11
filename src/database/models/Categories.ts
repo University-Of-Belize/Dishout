681402617efd8713d08fcbe44a40574d6ca92deb
AlexDev404 <alexdev404@users.noreply.github.com>
2023-11-11 17:54:21 -0600

    [FUTURE]: Category images

--- src/database/models/Categories.ts
+++ src/database/models/Categories.ts
@@ -18,6 +18,10 @@ const categoriesSchema = new Schema({
     type: String,
     required: true,
   },
+  // image: {
+  //   type: String,
+  //   required: true,
+  // },
   hidden: {
     type: Boolean,
     required: true,
