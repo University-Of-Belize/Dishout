a87f814670fe395c15fff772c3eeff2e92cc1e39
AlexDev404 <alexdev404@users.noreply.github.com>
2024-07-15 18:09:23 -0600

    Variation featureset complete. Next: Variation category feature-set

--- src/api/models/Admin/Menu/Variation/index.ts
+++ src/api/models/Admin/Menu/Variation/index.ts
@@ -1,16 +1,13 @@
 // Import our proper types
 import { Request, Response } from "express";
 // Import the schemas
-import Menu from "../../../../../database/models/Products";
-import ProductVariation from "../../../../../database/models/ProductVariation";
+import mongoose from "mongoose";
 import CatProductVariation from "../../../../../database/models/CatProductVariation";
+import ProductVariation from "../../../../../database/models/ProductVariation";
 import { ErrorFormat, iwe_strings } from "../../../../strings";
 import { get_authorization_user } from "../../../../utility/Authentication";
+import { wis_array } from "../../../../utility/What_Is";
 import what from "../../../../utility/Whats";
-import { what_is, wis_array } from "../../../../utility/What_Is";
-import { delete_object } from "../../../../utility/batchRequest";
-import settings from "../../../../../config/settings.json";
-import mongoose from "mongoose";
 
 // Create a new product variation
 async function variation_create(req: Request, res: Response) {
@@ -71,9 +68,7 @@ async function variation_create(req: Request, res: Response) {
   });
 
   await newVariation.save();
-  return res.json({
-    status: true,
-  });
+  return res.json(newVariation);
 }
 
 // Delete a product variation
@@ -121,7 +116,68 @@ async function variation_delete(req: Request, res: Response) {
 }
 
 // Modify a new product variation
-async function variation_modify(req: Request, res: Response) {}
+async function variation_modify(req: Request, res: Response) {
+  // We want to modify the product variation
+  // Note: URL is formatted as /api/admin/menu/variation/:variation_id
+  const variation_id = req.params.variation_id;
+  // Check our 'what_is'
+  if (req.body["what"] != what.private.variation) {
+    return res.status(418).send(iwe_strings.Generic.EFOLLOWRULES);
+  }
+
+  // Check our authentication token and see if it matches up to a staff member
+  const user = await get_authorization_user(req);
+  if (!user) {
+    return res
+      .status(403)
+      .json(ErrorFormat(iwe_strings.Authentication.EBADAUTH));
+  }
+
+  // Is this person a staff member?
+  if (!user.staff) {
+    return res
+      .status(403)
+      .json(ErrorFormat(iwe_strings.Authentication.ENOACCESS));
+  }
+
+  // Extract information from the 'what_is' object
+  const [variation_name, category_id] = wis_array(req);
+
+  // verify
+  const testFailed = check_values(res, variation_name, category_id);
+  if (testFailed) return;
+  const varIdFailed = check_values(res, "unused", variation_id);
+  if (varIdFailed) return;
+
+  // Check if a document with the same name and vcat_id already exists
+  const existing_variation = await ProductVariation.findById(variation_id);
+
+  if (!existing_variation) {
+    return res.status(404).json({
+      status: false,
+      message: iwe_strings.Product.Variation.ENOTFOUND,
+    });
+  }
+
+  // Check if the vcategory exists
+  const existing_category = await CatProductVariation.findById(category_id);
+  if (!existing_category) {
+    return res.status(400).json({
+      status: false,
+      message: iwe_strings.Product.Variation.Category.ENOTFOUND,
+    });
+  }
+
+  if (variation_name) {
+    existing_variation.Name = variation_name;
+  }
+  if (category_id) {
+    existing_variation.VCategory_id = category_id;
+  }
+
+  await existing_variation.save();
+  return res.json(existing_variation);
+}
 
 function check_values(
   res: Response,
